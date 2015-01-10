eaaf543931e48a1b1f995730d5d2be0e49efcfd1
jshurst <hurst1109@yahoo.com>
2015-01-10 15:41:28 -0500

    initial commit

---
+++ Consumer/Program.fs
@@ -0,0 +1,26 @@
+﻿open Microsoft.AspNet.SignalR
+open Microsoft.AspNet.SignalR.Client
+open Microsoft.AspNet.SignalR.Client.Hubs
+open System
+open System.Threading.Tasks
+
+[<EntryPoint>]
+let main argv = 
+    let connection = new HubConnection("http://localhost:8080")
+    let myHub = connection.CreateHubProxy "myHub"
+    connection.Start().Wait()
+    Console.ForegroundColor = ConsoleColor.Yellow |> ignore
+
+    myHub.Invoke<string>("chatter", "Hi!! Server").ContinueWith(fun (task : Task) -> 
+        if task.IsFaulted then 
+            Console.WriteLine("Could not Invoke the server method Chatter: {0}", task.Exception.GetBaseException())
+        else 
+            Console.WriteLine("Success calling chatter method"))
+    |> ignore
+
+    myHub.On<string>
+        ("addMessage", fun param -> Console.WriteLine("Client receiving value from server: {0}", param.ToString())) 
+    |> ignore
+    
+    Console.ReadLine() |> ignore
+    0
---
+++ Producer/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+open Microsoft.AspNet.SignalR
+open Microsoft.AspNet.SignalR.Hubs
+open Microsoft.Owin.Hosting
+open Microsoft.AspNet.SignalR.Owin
+open EkonBenefits.FSharp.Dynamic
+
+type public Startup() = 
+    member public this.Configuration(app) = 
+        let config = new HubConfiguration()
+        config.EnableDetailedErrors <- true
+        //Owin.OwinExtensions.MapSignalR(app);
+        Owin.MapExtensions.Map(app, "/signalr", 
+                               fun map -> 
+                                   Owin.CorsExtensions.UseCors(map, Microsoft.Owin.Cors.CorsOptions.AllowAll) |> ignore
+                                   Owin.OwinExtensions.RunSignalR(map, config))
+        |> ignore
+
+[<HubName("myHub")>]
+type public MyHub() = 
+    inherit Hub()
+    member public x.Chatter(param) = 
+        base.Clients.All?addMessage (param)
+        |> ignore
+
+[<EntryPoint>]
+let main argv = 
+    let url = "http://localhost:8080/"
+    use webApp = WebApp.Start<Startup>(url)
+    Console.ForegroundColor = ConsoleColor.Green |> ignore
+    Console.WriteLine("Server running on {0}", url)
+    Console.WriteLine("Press any key to start sending events to connected clients")
+    Console.ReadLine() |> ignore
+    let context : IHubContext = GlobalHost.ConnectionManager.GetHubContext<MyHub>()
+    for x in [ 0..1000 ] do
+        System.Threading.Thread.Sleep(1000)
+        Console.WriteLine("Server Sending Value to Client X: " + x.ToString())
+        context.Clients.All?addMessage (x.ToString())
+    Console.ReadLine() |> ignore
+    0
